6da0169534210b7d599737bd7bd28c02aad0dc44
Deep Saha <dip23447@gmail.com>
2025-12-23 08:31:02 +0530

    Added the initial workspace.

---
+++ src/main/java/com/deep/WIMB/WhereIsMyBusApplication.java
@@ -0,0 +1,13 @@
+package com.deep.WIMB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereIsMyBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereIsMyBusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deep/WIMB/WhereIsMyBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deep.WIMB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereIsMyBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
