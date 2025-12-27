6842980dfb9c467c02f12087b06e6d96c38dd749
Deep Saha <dip23447@gmail.com>
2025-12-27 22:05:42 +0530

    Added reverse geoencoding for extracting location name.

--- src/main/resources/static/js/track.js
+++ src/main/resources/static/js/track.js
@@ -15,6 +15,29 @@ const busIcon = L.divIcon({
 
 let marker;
 
+const locationCache = {};
+
+async function reverseGeocode(lat, lng) {
+  const key = `${lat.toFixed(4)},${lng.toFixed(4)}`;
+
+  if (locationCache[key]) return locationCache[key];
+
+  try {
+    const res = await fetch(
+      `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}`,
+      { headers: { "User-Agent": "WhereIsMyBus" } }
+    );
+
+    const data = await res.json();
+    const name = data.display_name || "Unknown location";
+    locationCache[key] = name;
+    return name;
+  } catch {
+    return "Location unavailable";
+  }
+}
+
+
 async function updateLocation() {
   const res = await fetch(
     `http://localhost:8080/api/location/last-loc/${rideId}`
@@ -27,14 +50,24 @@ async function updateLocation() {
 
   const pos = [loc.latitude, loc.longitude];
 
+  const locationName = await reverseGeocode(loc.latitude, loc.longitude);
+
   if (!marker) {
-    marker = L.marker(pos, {icon: busIcon}).addTo(map);
-  } else {
-    marker.setLatLng(pos);
-  }
+      marker = L.marker(pos, { icon: busIcon })
+        .addTo(map)
+        .bindPopup(`<b>Current Location:</b><br>${locationName}`);
+    } else {
+      marker.setLatLng(pos);
+      marker.setPopupContent(`<b>Current Location:</b><br>${locationName}`);
+    }
 
   map.setView(pos);
 }
+
+function goBack() {
+  window.history.back();
+}
+
 
 updateLocation();
 setInterval(updateLocation, 3000);
