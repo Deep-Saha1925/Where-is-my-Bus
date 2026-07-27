102870b19d4af438a5e9f5a04e7eba2df7c4c0aa
Deep-Saha1925 <dip23447@gmail.com>
2026-07-27 12:42:41 +0530

    Added Route.java

---
+++ src/main/java/com/deep/WIMB/model/Route.java
@@ -0,0 +1,28 @@
+package com.deep.WIMB.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Route {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String routeCode;   // e.g. "APD_FLK"
+
+    @Column(nullable = false)
+    private String routeName;
+
+    @Column(nullable = false)
+    private String filePath;    //  Excel file on disk
+}
