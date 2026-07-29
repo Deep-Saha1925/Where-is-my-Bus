daf3881b620965046bb18637db9d2851e0e7bf8f
Deep-Saha1925 <dip23447@gmail.com>
2026-07-30 00:02:51 +0530

    Updating RouteRepository

--- src/main/java/com/deep/WIMB/repository/RouteRepository.java
+++ src/main/java/com/deep/WIMB/repository/RouteRepository.java
@@ -3,9 +3,11 @@ package com.deep.WIMB.repository;
 import com.deep.WIMB.model.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RouteRepository extends JpaRepository<Route, Long> {
-    boolean existsByRouteCode(String routeCode);
     Optional<Route> findByRouteCode(String routeCode);
+    boolean existsByRouteCode(String routeCode);
+    List<Route> findAllByOrderByRouteNameAsc();
 }
