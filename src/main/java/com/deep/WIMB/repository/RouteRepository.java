f820e2ff139464e3562d6337cb19e8acaa2325c5
Deep-Saha1925 <dip23447@gmail.com>
2026-07-30 00:04:18 +0530

    Updating RouteRepository

--- src/main/java/com/deep/WIMB/repository/RouteRepository.java
+++ src/main/java/com/deep/WIMB/repository/RouteRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface RouteRepository extends JpaRepository<Route, Long> {
+
     Optional<Route> findByRouteCode(String routeCode);
     boolean existsByRouteCode(String routeCode);
     List<Route> findAllByOrderByRouteNameAsc();
