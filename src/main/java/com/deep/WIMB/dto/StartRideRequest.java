d2cdafce50e80e80c5489e4e720cff2870b5f00c
Deep-Saha1925 <dip23447@gmail.com>
2026-08-09 15:10:06 +0530

    Updating StartRideRequest

--- src/main/java/com/deep/WIMB/dto/StartRideRequest.java
+++ src/main/java/com/deep/WIMB/dto/StartRideRequest.java
@@ -9,6 +9,7 @@ public class StartRideRequest {
 
     private String busNumber;
     private String routeKey;
+    private String routeCode; // which registered route the driver picked; null/blank = legacy route
     private double latitude;
     private double longitude;
 }
